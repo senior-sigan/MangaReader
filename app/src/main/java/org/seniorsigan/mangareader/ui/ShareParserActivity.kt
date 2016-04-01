d36d3eed2e8ad8e93ea66e464af0213295495a92
Ilya Siganov <ilya.blan4@gmail.com>
2016-04-01 11:52:46 +0600

    fix build, add openin app feature

--- app/src/main/java/org/seniorsigan/mangareader/ui/ShareParserActivity.kt
+++ app/src/main/java/org/seniorsigan/mangareader/ui/ShareParserActivity.kt
@@ -4,10 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import okhttp3.HttpUrl
 import org.seniorsigan.mangareader.R
 import org.seniorsigan.mangareader.SHARED_URL
-import org.seniorsigan.mangareader.ui.ChapterActivity
-import org.seniorsigan.mangareader.ui.MainActivity
+import org.seniorsigan.mangareader.TAG
 import org.seniorsigan.mangareader.usecases.readmanga.ReadmangaParser
 
 class ShareParserActivity: AppCompatActivity() {
@@ -17,20 +17,34 @@ class ShareParserActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_share_parser)
 
-        var newIntent = Intent(this, MainActivity::class.java)
-
         if (intent.hasExtra(Intent.EXTRA_TEXT)) {
             val url = intent.getStringExtra(Intent.EXTRA_TEXT)
-            parser.extractPages(url, { pages ->
-                Log.d(org.seniorsigan.mangareader.TAG, "ShareParserActivity for $url find $pages")
-                if (pages.isNotEmpty()) {
-                    newIntent = Intent(this, ChapterActivity::class.java)
-                    newIntent.putExtra(SHARED_URL, pages.toTypedArray())
-                }
-
-                startActivity(newIntent)
-                finish()
-            })
+            parse(url)
+        } else if (intent.data != null) {
+            parse(intent.data.toString())
+        } else {
+            Log.d(TAG, "Nothing to do in ShareParserActivity")
+            finish()
         }
+    }
+
+    fun parse(urlRaw: String) {
+        val url = HttpUrl.parse(urlRaw)
+        if (url == null) {
+            Log.e(TAG, "Can't parse $urlRaw as url")
+            finish()
+            return
+        }
+        var newIntent = Intent(this, MainActivity::class.java)
+        parser.extractPages(url.toString(), { pages ->
+            Log.d(org.seniorsigan.mangareader.TAG, "ShareParserActivity for $url find $pages")
+            if (pages.isNotEmpty()) {
+                newIntent = Intent(this, ChapterActivity::class.java)
+                newIntent.putExtra(SHARED_URL, pages.toTypedArray())
+            }
+
+            startActivity(newIntent)
+            finish()
+        })
     }
 }
